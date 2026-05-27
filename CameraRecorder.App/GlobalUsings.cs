668e44967318ec19bd9c645eb12d601a4f608a57
for7raid <alexey@for7raid.ru>
2026-05-27 20:58:17 +0300

    добавлено описание удалил проект MAUI

--- CameraRecorder.App/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-global using CameraRecorder.App.PageModels;
-global using CameraRecorder.App.Pages;
-global using CameraRecorder.App.Services;
-global using CameraRecorder.App.Utilities;
-global using Fonts;
--- CameraRecorder.App/Services/ISettingsStorageService.cs
+++
@@ -1,9 +0,0 @@
-using CameraRecorder.Settings;
-
-namespace CameraRecorder.App.Services;
-
-public interface ISettingsStorageService
-{
-    Task<CameraRecorderSettings> LoadAsync();
-    Task SaveAsync(CameraRecorderSettings settings);
-}
